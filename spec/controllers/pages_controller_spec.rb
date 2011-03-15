8f5609c5ced33295189079def04d7093ab3951fc
Vin Turk <vinceturk@gmail.com>
2011-03-14 21:02:18 -0400

    added title helper and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,8 +15,8 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", 
+                                    :content => "#{@base_title} | Home")
     end
   end
 
@@ -25,7 +29,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
     
     it "should have a non-blank body" do
@@ -43,7 +47,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => "#{@base_title} | About")
     end
     
   end
