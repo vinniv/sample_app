93ac1976228e6cdd6d983f10a4291b8a3eacf6ba
Vin Turk <vinceturk@gmail.com>
2011-03-15 19:17:16 -0400

    fixed the annoying erros

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -32,10 +32,6 @@ describe PagesController do
                                     :content => "#{@base_title} | Contact")
     end
     
-    it "should have a non-blank body" do
-      get 'home'
-      response.body.should_not =~ /<body>\s*<\/body>/
-    end
   end
   
   describe "GET 'about'" do
@@ -49,7 +45,19 @@ describe PagesController do
       response.should have_selector("title",
                                     :content => "#{@base_title} | About")
     end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
     
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Help")
+    end
   end
 
 end
